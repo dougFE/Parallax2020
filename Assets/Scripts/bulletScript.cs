4042b0aa40d6a629a03fa0beb910b0bcc45be7d3
caolinnh <50995312+caolinnh@users.noreply.github.com>
2020-11-21 13:52:33 -0800

    Add files via upload

---
+++ Assets/Scripts/bulletScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+	private Vector3 normalizeDirection;
+	public Transform Target;
+
+    	public float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	normalizeDirection = (Target.position - transform.position).normalized;
+    	
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+       transform.position += normalizeDirection * speed * Time.deltaTime;
+       
+       
+    }
+}
